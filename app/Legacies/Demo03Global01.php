731124c989824c11e5aeb8e64c336628da6716cc
mouson chen <mouson@gmail.com>
2020-07-24 01:04:02 +0800

    Demo03 de Global 01 with empty string

--- app/Legacies/Demo03Global01.php
+++ app/Legacies/Demo03Global01.php
@@ -4,6 +4,13 @@ return global_demo01();
 
 function global_demo01()
 {
+    /**
+     * @ref https://www.php.net/manual/en/security.globals.php
+     * 在做這波修改的時期，必須要考慮相容 PHP5
+     * 因此沒使用 ?? or ?: 這個更簡短的用法
+     * $name = $_POST['name'] ?? '';
+     */
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
 
     $output = "Hi, {$name}";
 
