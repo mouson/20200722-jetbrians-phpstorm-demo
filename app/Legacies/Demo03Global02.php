c1ab99b652b26963d48a6ae66c034a7677ddc545
mouson chen <mouson@gmail.com>
2020-07-24 01:04:41 +0800

    Demo03 de Global 02 with comparison is set pattern

--- app/Legacies/Demo03Global02.php
+++ app/Legacies/Demo03Global02.php
@@ -2,7 +2,15 @@
 if (function_exists('global_demo02') == false) {
     function global_demo02()
     {
-
+        /**
+         * @ref https://www.php.net/manual/en/security.globals.php
+         * @ref https://www.php.net/manual/en/types.comparisons.php
+         *
+         * 在做這波修改的時期，必須要考慮相容 PHP5
+         * 因此沒使用 ?? or ?: 這個更簡短的用法
+         * $name = $_POST['name'] ?? '';
+         */
+        $name = isset($_POST['name']) ? $_POST['name'] : null;
 
         if (isset($name)) {
             $output = "Hi, {$name}";
